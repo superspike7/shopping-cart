c398fbf1b947ed6829b5818400d92768347048f4
Spike Vinz Cruz <spikevinz@gmail.com>
2021-09-19 08:04:20 +0800

    add cart state

--- src/Shop.jsx
+++ src/Shop.jsx
@@ -1,19 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { items } from "./data";
 import ItemCard from "./ItemCard";
 
-export default function Shop(){
-  return(
+export default function Shop() {
+  const [cart, setCart] = useState([]);
+
+  function handleAddCart(id) {
+    setCart([...cart, getItemById(id) ])
+  }
+
+  useEffect(()=>{
+    console.log(cart)
+  },[cart])
+
+  function getItemById(id) {
+    return items.find((item) => id === item.id);
+  }
+
+  return (
     <div className="">
       <nav className="text-center flex justify-center gap-5 mt-1 bg-blue-100">
         <h3>Items: 0</h3>
-        <button className="bg-green-500 rounded-md text-white px-1" >Checkout</button>
+        <button className="bg-green-500 rounded-md text-white px-1">
+          Checkout
+        </button>
       </nav>
       <div className="w-1/2 mx-auto grid grid-cols-4 gap-5 mt-10">
-        {items.map(item=>(
-          <ItemCard name={item.name} price={item.price} key={item.id} />
+        {items.map((item) => (
+          <ItemCard
+            name={item.name}
+            price={item.price}
+            key={item.id}
+            id={item.id}
+            handleClick={handleAddCart}
+          />
         ))}
       </div>
     </div>
-  )
+  );
 }
