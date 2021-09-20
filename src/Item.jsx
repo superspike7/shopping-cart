6bc3591cf38a1f2f0e2fd6fdc1d97bc7c429e835
Spike Vinz Cruz <spikevinz@gmail.com>
2021-09-20 08:39:53 +0800

    style item page

--- src/Item.jsx
+++ src/Item.jsx
@@ -14,25 +14,42 @@ export default function Item({ handleAddToCart }) {
     setItem(findItem);
   }, []);
 
-  useEffect(()=>{
-    setCartItems(new Array(amount).fill(item))
-  },[amount, item])
+  useEffect(() => {
+    setCartItems(new Array(amount).fill(item));
+  }, [amount, item]);
 
   function handleChange(e) {
-    setAmount(Number(e.target.value))
+    setAmount(Number(e.target.value));
+  }
+
+  function getImageUrl(iconId) {
+    return new URL(`./Icons/${iconId}.svg`, import.meta.url).href;
   }
 
   return (
-    <div className="w-1/2 mt-10 mx-auto justify-center flex flex-col items-center">
-      <div className="bg-blue-200 flex justify-center items-center w-64 h-64 ">
-        image
+    <div className="w-1/2 mt-10 mx-auto justify-center flex items-center">
+      <div className="bg-blue-100 rounded-xl flex justify-center items-center w-64 h-64 p-5">
+        <img src={getImageUrl(item.id)} alt="" />
       </div>
-      <h1>{item.name}</h1>
-      <h1>{item.price}</h1>
-      <form onSubmit={(e) => handleAddToCart(e, cartItems)}>
-        <input type="number" value={amount} onChange={handleChange} />
-        <button>Add to Cart</button>
-      </form>
+      <div className="flex flex-col justify-around text-center h-64">
+        <h1 className="text-4xl font-bold">{item.name}</h1>
+        <h1 className="text-2xl font-light">{item.price}</h1>
+        <form
+          onSubmit={(e) => handleAddToCart(e, cartItems)}
+          className="flex flex-col items-center justify-center gap-3"
+        >
+          <input
+            type="number"
+            value={amount}
+            onChange={handleChange}
+            className="w-1/4 text-2xl bg-gray-100 p-1 rounded-md text-center "
+            min="0"
+          />
+          <button className="text-xl bg-blue-500 rounded-md text-white font-bold px-2">
+            Add to Cart
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
