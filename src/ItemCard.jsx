8e2a08cd39ddd99cfec6741afb86ee43939c608a
Spike Vinz Cruz <spikevinz@gmail.com>
2021-09-20 08:21:29 +0800

    add icons

--- src/ItemCard.jsx
+++ src/ItemCard.jsx
@@ -2,19 +2,24 @@ import React from "react";
 import { Link, useRouteMatch } from "react-router-dom";
 
 export default function ItemCard(props) {
-  const { name, price, id} = props;
+  const { name, price, id } = props;
   let { url } = useRouteMatch();
 
+  function getImageUrl(iconId) {
+    return new URL(`./Icons/${iconId}.svg`, import.meta.url).href;
+  }
+
   return (
-    <div className="bg-gray-100 text-gray-900 flex flex-col justify-center items-center p-2 gap-2">
-      <div className="bg-blue-500 h-32 w-32 flex items-center justify-center text-white">
-        image
+    <Link to={`${url}/${id}`} className="hover:border-4 border-green-500">
+      <div className="bg-gray-100 text-gray-900 flex flex-col justify-center text-center items-center p-2 gap-4">
+        <div className="bg-blue-100 p-5 rounded-xl h-32 w-32 flex items-center justify-center text-white">
+          <img src={getImageUrl(id)} alt="" />
+        </div>
+        <h1 className="text-2xl font-semibold">{name}</h1>
+        <p className="font-bold text-white px-2 rounded-md bg-blue-500">
+          {price}
+        </p>
       </div>
-      <h1>{name}</h1>
-      <p>{price}</p>
-      <Link to={`${url}/${id}`} className="bg-blue-500 text-white font-bold px-2 rounded-md">
-        more info 
-      </Link>
-    </div>
+    </Link>
   );
 }
--- src/data.js
+++ src/data.js
@@ -1,12 +1,122 @@
 export const items = [
-  { id: 1, name: "1", price: 69.99 },
-  { id: 2, name: "2", price: 19.99 },
-  { id: 3, name: "3", price: 29.99 },
-  { id: 4, name: "4", price: 49.99 },
-  { id: 5, name: "5", price: 89.99 },
-  { id: 6, name: "6", price: 79.99 },
-  { id: 7, name: "7", price: 39.99 },
-  { id: 8, name: "8", price: 67.99 },
-  { id: 9, name: "9", price: 169.99 },
-  { id: 10, name: "10", price: 99.99 },
+  {
+    id: 1,
+    name: "cupcake",
+    price: 69.99,
+  },
+  {
+    id: 2,
+    name: "ice cream",
+    price: 69.99,
+  },
+  {
+    id: 3,
+    name: "double pop",
+    price: 69.99,
+  },
+  {
+    id: 4,
+    name: "lolipop",
+    price: 69.99,
+  },
+  {
+    id: 5,
+    name: "donut",
+    price: 69.99,
+  },
+  {
+    id: 6,
+    name: "churros",
+    price: 69.99,
+  },
+  {
+    id: 7,
+    name: "jelly",
+    price: 69.99,
+  },
+  {
+    id: 8,
+    name: "cotton candy",
+    price: 69.99,
+  },
+  {
+    id: 9,
+    name: "tiramisu",
+    price: 69.99,
+  },
+  {
+    id: 10,
+    name: "gummy worms",
+    price: 69.99,
+  },
+  {
+    id: 11,
+    name: "gummy bear",
+    price: 69.99,
+  },
+  {
+    id: 12,
+    name: "cake",
+    price: 69.99,
+  },
+  {
+    id: 13,
+    name: "candy",
+    price: 69.99,
+  },
+  {
+    id: 14,
+    name: "chocolate",
+    price: 69.99,
+  },
+  {
+    id: 15,
+    name: "pancake",
+    price: 69.99,
+  },
+  {
+    id: 16,
+    name: "croissant",
+    price: 69.99,
+  },
+  {
+    id: 17,
+    name: "pie",
+    price: 69.99,
+  },
+  {
+    id: 18,
+    name: "strawberry jam",
+    price: 69.99,
+  },
+  {
+    id: 19,
+    name: "sundae",
+    price: 69.99,
+  },
+  {
+    id: 20,
+    name: "waffles",
+    price: 69.99,
+  },
+  {
+    id: 21,
+    name: "biscuit",
+    price: 69.99,
+  },
+  {
+    id: 22,
+    name: "pudding",
+    price: 69.99,
+  },
+  {
+    id: 23,
+    name: "wafer",
+    price: 69.99,
+  },
+  {
+    id: 24,
+    name: "hot chocolate",
+    price: 69.99,
+  },
 ];
